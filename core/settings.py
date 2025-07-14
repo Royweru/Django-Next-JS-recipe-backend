2c2b720c6d5085ecfdd704a375c7e4493993d6f9
Royweru <royezi17@gmail.com>
2025-07-14 21:22:01 +0300

    Added the frontend url to the list of allowed recipes

--- core/settings.py
+++ core/settings.py
@@ -168,6 +168,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://recipeshare-eta.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
