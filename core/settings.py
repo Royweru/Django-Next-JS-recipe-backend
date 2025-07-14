3feab9138d6df473d7be915101064d6382ca238d
Royweru <royezi17@gmail.com>
2025-07-14 08:12:28 +0300

    Added the url to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '0.0.0.0','recipe-share-api-nmwa.onrender.com']
 
 
 DJANGO_APPS = [
